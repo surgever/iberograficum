31385b91f9ea36d1a041c86ab92c1f97b5f2bfd3
Sergio <surgever.com@gmail.com>
2023-12-27 07:42:28 +0100

    Removed epi nav

--- src/routes/Epi.jsx
+++ src/routes/Epi.jsx
@@ -4,10 +4,6 @@ import { NavLink, Outlet } from 'react-router-dom';
 const Epi = () => {
     return (
       <>
-        <nav className="navbar">
-          <NavLink to="ullastret">Plomo de Ullastret</NavLink>
-          <NavLink to="letreros"> Vaso de los letreros</NavLink>
-        </nav>
         <Outlet />
       </>
     );
