cfaa85faf4c6f7f7e3f015180d0b8be98f8b508d
Sergio Oliver <yo@surgever.com>
2024-06-06 12:58:54 +0200

    Extracted component Transcription from Plomo

--- src/components/Plomo.jsx
+++ src/components/Plomo.jsx
@@ -1,6 +1,6 @@
 import PlomoUllastret from "@/img/C.2.3 Ullastret - Foto.png"
-import PlomoPrat from "@/img/C.2.3 Ullastret - O.Prat.png"
 import { useParams } from 'react-router-dom';
+import Transcription from '@/components/Transcription';
 
 const Plomo = props => {
     let { code } = useParams();
@@ -20,36 +20,7 @@ const Plomo = props => {
                 <img className="transition" src={PlomoUllastret} alt="Plomo Ullastret"/>
             </a></figure>
             <h2>{title == 'No encontrado' ? code : title}</h2>
-            <pre>{epiText}</pre>
-            <div className="wrap">
-                <table><tbody>
-                    <tr><th colSpan="2"><img src={PlomoPrat} alt="Calco Plomo Ullastret" /></th></tr>
-                    <tr>
-                        <td className="ibero"><p>𐈀𐈬​:​𐈊𐈯𐈅𐈀𐈭𐈂𐈋</p></td>
-                        <td className="latin">ar : basiařebe aasdas</td>
-                    </tr>
-                    <tr>
-                        <td className="ibero">𐈂𐈊𐈮𐈅𐈐𐈧𐈂​:​𐈣𐈅𐈓𐈯𐈅𐈮𐈀​:​𐈍𐈬𐈯𐈝​:​𐈀𐈊𐈮𐈔𐈍𐈬𐈯𐈝​:​𐈝𐈮</td>
-                        <td className="latin">ebaŕikame : tuikesiŕa : borste : abaŕgeborste : teŕ</td>
-                    </tr>
-                    <tr>
-                        <td className="ibero">𐈠𐈭𐈯​:​𐈊𐈅𐈞𐈯𐈌​:​𐈩𐈂𐈅𐈝𐈔𐈭𐈉​:​𐈛𐈮𐈋𐈥𐈅𐈇𐈭𐈙​:​𐈟𐈧𐈇𐈭</td>
-                        <td className="latin">diřs : baidesbi : neitegeřu : taŕbeliořku : timoř</td>
-                    </tr>
-                    <tr>
-                        <td className="ibero">𐈕𐈮​:​𐈊𐈬𐈜𐈱𐈗​:​𐈀𐈩𐈋𐈅𐈙​:​𐈊𐈅𐈞𐈯𐈅𐈬​:​𐈯𐈀𐈥𐈤𐈗​:​𐈙𐈥𐈂𐈛𐈋𐈭𐈙+[ &nbsp;</td>
-                        <td className="latin">kiŕ : bardaśko : anbeiku : baidesir : salduko : kuletabeřku+[</td>
-                    </tr>
-                    <tr>
-                        <td className="ibero">𐈌𐈖𐈥𐈠𐈭𐈯𐈝​:​𐈂𐈮𐈂𐈱𐈉​:​𐈗𐈠𐈊𐈩𐈂𐈩​:​𐈂𐈋𐈬𐈑​:​𐈛𐈱𐈐𐈥𐈅𐈮𐈯</td>
-                        <td className="latin">bigildiřste : eŕeśu : kodibanen : eberga : taśkaliŕs</td>
-                    </tr>
-                    <tr>
-                        <td className="ibero">&nbsp; 𐈥𐈇𐈭𐈯𐈀​:​𐈊𐈟𐈌​:​𐈌𐈉𐈭𐈛𐈩𐈂𐈱​:​𐈯𐈀𐈥𐈤𐈖𐈥𐈂𐈭𐈙​:​𐈖𐈿</td>
-                        <td className="latin">&nbsp; &nbsp; lořsa : batibi : biuřtaneś : saldugileřku : gi</td>
-                    </tr>
-                </tbody></table>
-            </div>
+            <Transcription epiText={epiText} code={code} />
         </div>
     )
 }
