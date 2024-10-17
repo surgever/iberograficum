a2806bab15f69f49f234745ba8a68dc387848e45
Sergio Oliver <yo@surgever.com>
2024-10-17 14:35:46 +0200

    Footer adjusts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = props => {
             <li><a href="https://www.academia.edu/37179345/L_origen_i_el_desenvolupament_de_les_escriptures_paleohispàniques" target="_blank">L’origen i el desenvolupament
                 de les escriptures paleohispàniques</a>, Joan Ferrer i Jané, 2018 </li>
             <li><a href="https://ifc.dpz.es/publicaciones/ebooks/id/3649" target="_blank">Acta Palehispanica XII. Revista sobre lenguas y culturas de la Hispania Antigua, 2017</a> </li>
-            <li>Tú, por visitar este sitio :D</li>
+            <li>Tú, por usar esta app :D</li>
             <li style={{listStyle: "none", textAlign: "center"}}>
             <hr />
             Desarrollado por <a href="https://surgever.com" target="_blank">Sergio&nbsp;Oliver</a>.
