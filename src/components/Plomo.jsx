3fd85388a2aaa02db7a3776c478f34a17e75e788
Sergio Oliver <yo@surgever.com>
2025-07-16 16:33:41 +0200

    Code search with results

--- src/components/Plomo.jsx
+++ src/components/Plomo.jsx
@@ -9,7 +9,8 @@ import SinarcasCalco from "@/img/Estela de Sinarcas - Calco.gif"
 import Letreros from "@/img/vaso-letreros.png"
 import LetrerosCalco from "@/img/vaso-letreros-calco.png"
 
-import { useParams } from 'react-router-dom';
+import { useState } from "react"
+import { useParams, NavLink } from 'react-router-dom';
 import Transcription from '@/components/Transcription';
 import Infobox from "@/components/Infobox"
 
@@ -46,9 +47,36 @@ const Plomo = props => {
     const { title, description } = epiContent;
     let HesperiaURL = 'http://hesperia.ucm.es/consulta_hesperia/inscripciones_localidad_zonas_C_y_D.php?ref=' + code;
 
+    // Search by code
+    const [searchOpen, setSearchOpen] = useState(false);
+    const [searchText, setSearchText] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+    const handleSearchCode = newText => {
+        setSearchText(newText.toUpperCase())
+        // Search epiDB if 4 chars or more are filled in
+        if(newText.length > 1) {
+            // Search {newText} in the keys of epiDB.epis[key, reading] 
+            let searchArray = [];
+            for(let key in epiDB.epis) {
+                if(key.toUpperCase().indexOf(newText.toUpperCase())!=-1) {
+                    searchArray.push({key, 'text' : epiDB.epis[key]}  );
+                }
+            }
+            setSearchResults(searchArray);
+        } else if(newText.length === 0) {
+            setSearchResults([]);
+        }
+    }
+    const handleCloseSearch = e => {
+        e.preventDefault()
+        setSearchOpen( false )
+        setSearchText('')
+        setSearchResults([])
+    }
+
     return (
         
-        <div className="example">
+        <div className={"example searchcode " + (searchOpen ? 'on' : 'off')}>
             {epiImg[code] && epiImg[code].photo &&
                 <figure id="plom"><a href={epiImg[code].photo} target="_blank" rel="nofollow noreferrer">
                     <img className="transition" src={epiImg[code].photo} alt="Plomo "/>
@@ -56,7 +84,29 @@ const Plomo = props => {
             }
             <h2>
                 {title != 'No encontrado' && title}
-                <input className="hespCode" type="text" value={(epiDB.epis[code] ? code : '404')} />
+                <input type="text" className="hespCode"
+                    placeholder={ searchOpen ? 'Search code' : 'Search...'}
+                    onFocus={ () => setSearchOpen( true ) }
+                    onBlur={ () => searchResults.length == 0 && setSearchOpen( false ) }
+                    onChange={e => handleSearchCode(e.target.value)}
+                    value={ searchOpen ? searchText : (epiDB.epis[code] ? code : '404')}
+                />
+                { searchText && searchResults.length >= 1 &&
+                <li className='clear-li'>
+                    <button onClick={handleCloseSearch}>✕</button>
+                </li>
+                }
+                <ul className="results">
+                    {searchResults.map((epi) => {
+                        return (
+                            <li key={epi.key}>
+                                <NavLink to={'../'+epi.key} >
+                                    <span>{epi.key}</span>
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
             </h2>
             <Infobox></Infobox>
             {epiDB.epis[code] &&
